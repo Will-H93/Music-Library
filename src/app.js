9965739569bc3577542dafe342ee14b12a1ed3b6
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-08-03 21:06:46 +0100

    Dotenv changes

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (_, res) => {
+    res.status(200).json({ result: "Hello World" })
+})
+
+module.exports = app;
