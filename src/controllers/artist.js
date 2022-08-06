abbc4d48f7aba5b4b90a1e3d3b62a83f424f45f9
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-08-06 13:03:57 +0100

    Can add artists to table via postman

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,18 +1,19 @@
 const getDb = require('../services/db');
 
-const newArtist = async(_, res) => {
+const newArtist = async(req, res) => {
     
     const db = await getDb();
-    const { name, genre } = res.body;
+    const { name, genre } = req.body;
 
     try {
     await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}');`);
-    db.end();
+    
     return res.status(201).json({ message: "POST new artist" });
     } catch (error) {
-        db.end();
-        return res.status(500).json({ message: "error" })
+        
+        res.status(500).json({ message: "error" })
     }
+    db.end();
 }
 
 module.exports = { newArtist }
