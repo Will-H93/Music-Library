4988f6b1190803f552775dd2e99a894db07824e8
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-08-06 12:28:52 +0100

    added db.js

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,5 +1,18 @@
-const newArtist = (_, res) => {
-    res.status(201).json({ message: "POST new artist" });
+const getDb = require('../services/db');
+
+const newArtist = async(_, res) => {
+    
+    const db = await getDb();
+    const { name, genre } = res.body;
+
+    try {
+    await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}');`);
+    db.end();
+    return res.status(201).json({ message: "POST new artist" });
+    } catch (error) {
+        db.end();
+        return res.status(500).json({ message: "error" })
+    }
 }
 
 module.exports = { newArtist }
