ea3223ae8c6a393aa0575738208fa62e1c91ec74
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-08-14 11:05:12 +0100

    removed comments from drop-database file

--- utils/drop-database.js
+++ utils/drop-database.js
@@ -1,21 +1,16 @@
-// require the promise version of mysql2
 const mysql = require('mysql2/promise');
 
-// require path to handle file paths
 const path = require('path');
 
-// load environment variables from env files
 require('dotenv').config({
   path: path.join(__dirname, '../.env.test'),
 });
 
-// destructure environment variables from process.env
 const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
 
-// This asyncronous function will run before app
 const dropDatabase = async () => {
   try {
-    // connect to the database
+
     const db = await mysql.createConnection({
       host: DB_HOST,
       user: DB_USER,
@@ -23,11 +18,9 @@ const dropDatabase = async () => {
       port: DB_PORT,
     });
 
-    // drop the database
     await db.query(`DROP DATABASE ${DB_NAME}`);
     db.end();
   } catch (err) {
-    // if something goes wrong, console.log the error and the current environment variables
     console.log(
       `Your environment variables might be wrong. Please double check .env file`
     );
@@ -42,5 +35,4 @@ const dropDatabase = async () => {
   }
 };
 
-// run the async function
 dropDatabase();
